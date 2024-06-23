9bc9034233f2dd8ef0db5147efa00140f03002ea
iamnashir <nashir8609@gmail.com>
2024-06-23 18:40:17 +0400

    version 2.0

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // JavaScript code can be added here
+$(document).ready(function() {
+    $('#menu-btn').click(function() {
+        $('#mobile-menu').removeClass('hidden');
+    });
+
+    $('#close-btn').click(function() {
+        $('#mobile-menu').addClass('hidden');
+    });
+
+    // Close the sidebar when clicking outside of it
+    $(window).click(function(e) {
+        if ($(e.target).is('#mobile-menu')) {
+            $('#mobile-menu').addClass('hidden');
+        }
+    });
 });
