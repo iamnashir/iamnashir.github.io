133724cc60d9b41ba0d62cbf766bb5fc3c98dc82
iamnashir <nashir8609@gmail.com>
2024-06-23 18:48:55 +0400

    minor change

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
     $('#menu-btn').click(function() {
-        $('#mobile-menu').removeClass('hidden');
+        $('#mobile-menu').css('left', '0');
     });
 
     $('#close-btn').click(function() {
-        $('#mobile-menu').addClass('hidden');
+        $('#mobile-menu').css('left', '-100%');
     });
 
     // Close the sidebar when clicking outside of it
     $(window).click(function(e) {
         if ($(e.target).is('#mobile-menu')) {
-            $('#mobile-menu').addClass('hidden');
+            $('#mobile-menu').css('left', '-100%');
         }
     });
 });
