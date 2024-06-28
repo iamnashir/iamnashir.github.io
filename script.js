7a3facdd80073172fbbf362eef7f9a0d5ca84d84
iamnashir <nashir8609@gmail.com>
2024-06-28 19:41:21 +0400

    version 10.1

--- script.js
+++ script.js
@@ -1,70 +1,14 @@
-$(document).ready(function() {
-    // Infinite loop for certificates carousel
-    setInterval(function() {
-        $('.certificates-carousel').animate({ marginLeft: '-=220px' }, 1000, function() {
-            $(this).find('.certificate-card:first').appendTo($(this));
-            $(this).css('margin-left', '0');
-        });
-    }, 3000);
-
-    // Infinite loop for skills carousel
-    setInterval(function() {
-        $('.skills-carousel').animate({ marginLeft: '-=220px' }, 1000, function() {
-            $(this).find('.skill-card:first').appendTo($(this));
-            $(this).css('margin-left', '0');
-        });
-    }, 3000);
-    
-    document.addEventListener('DOMContentLoaded', function () {
-    function isInViewport(element) {
-        const rect = element.getBoundingClientRect();
-        return (
-            rect.top < (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.bottom > 0
-        );
-    }
-
-    function addAnimationClass() {
-        const timelineItems = document.querySelectorAll('.timeline-item');
-        timelineItems.forEach(function (item) {
-            if (isInViewport(item)) {
-                item.classList.add('animate');
-            }
-        });
-    }
-    window.addEventListener('scroll', addAnimationClass);
-    window.addEventListener('resize', addAnimationClass);
-    addAnimationClass(); // Initial check on page load
+const btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+  document.querySelector("header ul").classList.toggle("show");
 });
 
-    document.addEventListener('DOMContentLoaded', function () {
-    function animateHeroContent() {
-        const heroTitle = document.querySelector('.hero-title');
-        const heroDescription = document.querySelector('.hero-description');
-        const heroImage = document.querySelector('.hero-image');
+const toTop = document.querySelector(".back-top");
 
-        heroTitle.classList.add('animate__animated', 'animate__fadeInDown');
-        heroDescription.classList.add('animate__animated', 'animate__fadeInUp');
-        heroImage.classList.add('animate__animated', 'animate__zoomIn');
-    }
-
-    animateHeroContent(); // Initial animation on page load
-});
-
-    //mobile menu 
-    document.getElementById('open-menu-btn').addEventListener('click', function() {
-    document.getElementById('mobile-menu').classList.add('active');
-});
-
-document.getElementById('close-btn').addEventListener('click', function() {
-    document.getElementById('mobile-menu').classList.remove('active');
-});
-
-document.getElementById('mobile-menu').addEventListener('click', function(event) {
-    if (event.target === this) {
-        document.getElementById('mobile-menu').classList.remove('active');
-    }
-});
-
-
+window.addEventListener("scroll", () => {
+  if (pageYOffset > 75) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
 });
